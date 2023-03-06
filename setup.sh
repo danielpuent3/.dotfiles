571737fd33d94c6f76ae52c212fb7b68ff2e7985
Daniel Puente <daniel.puente.dev@gmail.com>
2023-03-06 12:11:16 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #install oh-my-zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#echo "source ~/.dotfiles/.zshrc_personal" >> ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "source ~/.dotfiles/.zshrc_personal" >> ~/.zshrc
 
 
 #install homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#brew install --cask hyper
-#hyper i hyper-snazzy
+brew install --cask hyper
+hyper i hyper-snazzy
 
-#brew install gh
+brew install gh
 
-#brew install zsh
-#$(brew --prefix)/bin/zsh
+brew install zsh
+$(brew --prefix)/bin/zsh
 
 brew install tmux
 
 brew install fig
 
-#cd ~/.dotfiles
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+cd ~/.dotfiles
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
